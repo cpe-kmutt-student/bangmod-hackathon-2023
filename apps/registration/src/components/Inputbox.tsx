13a20de02c2b506168d3fe31bf24490fd73dd857
Encall <apichataimimpat@gmail.com>
2022-10-06 01:28:11 +0700

    Fix generic-input-box (#22)
    * create generic string input
    
    * redo inputbox
    
    * fixed
    
    * Delete logo01.png
    
    * fixed again
    
    Co-authored-by: Krid Heprakhone <riflowth@gmail.com>

--- apps/registration/src/components/Inputbox.tsx
+++ apps/registration/src/components/Inputbox.tsx
@@ -11,9 +11,12 @@ const Inputbox = ({
   placeholder?: string;
   required?: boolean;
 }) => {
-  const handleChange = (e) => {
-    if (setObj) setObj(e.target.value);
+  const handleChange = (event: Event) => {
+    if (!(event.target instanceof HTMLInputElement)) return;
+    const value = event.target.value;
+    setObj(value);
   };
+
 
   return (
     <div class="mb-6">
@@ -32,7 +35,7 @@ const Inputbox = ({
         type="text"
         placeholder={placeholder}
         value={obj}
-        onChange={handleChange}
+        onInput={handleChange}
         className="rounded-md appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm"
       />
     </div>
