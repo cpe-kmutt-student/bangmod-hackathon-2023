f79fcccb35b99b1e7484d9b5f2912a5302a59405
Jirapat <bukharney@gmail.com>
2022-10-08 22:41:50 +0700

    final

--- apps/registration/src/App.tsx
+++ apps/registration/src/App.tsx
@@ -1,11 +1,11 @@
-import { Welcome } from '@/components/Welcome';
+import { HomePage } from '@/pages/HomePage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
     <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
--- apps/registration/src/components/ImageInputBox.tsx
+++ apps/registration/src/components/ImageInputBox.tsx
@@ -8,12 +8,15 @@ export const ImageInputBox = (prop:{
 
     //file in this obj
     const [files, setFiles] = useState(null)
-
+    const hText = "text-xl text-violet-900 antialiased"
+    const pText = "px-4 text-violet-900 antialiased text-lg"
+    const button = "p-2 m-2 text-white rounded-full border-solid focus:outline-none focus:ring"
+    const border = "flex flex-col justify-center items-center h-60 border-dashed rounded-2xl border-2 border-violet-500 m-5 hover:border-violet-600 bg-white drop-shadow-lg"
     const inputRef: any = useRef(null)
     const handleDragOver = (event:DragEvent)=>{
         event.preventDefault()
     }
-    const handleDrop = (event:any)=>{
+    const handleDrop = (event:any)=>{ 
         event.preventDefault()
         setFiles(event.dataTransfer.files)
     }
@@ -36,19 +39,17 @@ export const ImageInputBox = (prop:{
         <div className='grid grid-cols-1 md:grid-cols-2'>
             <div className='flex flex-col 
                             justify-start items-start h-30 m-5'>
-                <h1 className='text-xl text-violet-900 antialiased'>แนบไฟล์เอกสาร*</h1>
+                <h1 className={hText}>แนบไฟล์เอกสาร*</h1>
                 <ul>
                     {Array.from(name).map((name, idx) => {
                         if(name != ''){
-                            return <li className='px-4 text-violet-900 antialiased text-lg' 
+                            return <li className={pText} 
                         key = {idx}>{idx+1}<span>{". "}</span>{name}</li>
                         }}   
                     )}
                  </ul>
             </div>
-            <div className='flex flex-col 
-                        justify-center items-center h-60 border-dashed rounded-3xl border-2 
-                        border-violet-500 m-5 hover:border-violet-400 bg-white drop-shadow-lg'>
+            <div className={border}>
                 <div class='m-5 flex flex-col 
                         justify-center items-center'>
                     <ul>
@@ -57,13 +58,9 @@ export const ImageInputBox = (prop:{
                         )}
                     </ul>
                 <div class='py-2'>
-                        <button class='p-2 mr-5 text-white rounded-full border-solid bg-gray-500 
-                        hover:bg-red-500 active:bg-red-400
-                        focus:outline-none focus:ring focus:ring-red-300'
+                        <button class={button+"focus:ring-red-300 bg-gray-500 hover:bg-red-500 active:bg-red-400"}
                         onClick={() => setFiles(null)}>Cancel</button>
-                        <button class='p-2 text-white rounded-full border-solid bg-violet-500 
-                        hover:bg-violet-600 active:bg-violet-700 
-                        focus:outline-none focus:ring focus:ring-violet-300'
+                        <button class={button+"focus:ring-violet-300 bg-violet-500 hover:bg-violet-600 active:bg-violet-700"}
                         onClick={handleUpload}>Upload</button>
                 </div>
                 </div>
@@ -75,20 +72,18 @@ export const ImageInputBox = (prop:{
         <div className='grid grid-cols-1 md:grid-cols-2'>
             <div className='flex flex-col 
                         justify-start items-start h-30 m-5'>
-                <h1 className='text-xl text-violet-900 antialiased'>แนบไฟล์เอกสาร*</h1>
+                <h1 className={hText}>แนบไฟล์เอกสาร*</h1>
                 <ul>
                     {Array.from(name).map((name, idx) => {
                         if(name != ''){
-                            return <li className='px-4 text-violet-900 antialiased text-lg' 
+                            return <li className={pText}
                         key = {idx}>{idx+1}<span>{". "}</span>{name}</li>
                         }}   
                     )}
                 </ul>
             </div>
             {!files && (
-                <div className='flex flex-col 
-                justify-center items-center h-60 border-dashed rounded-3xl border-2 
-                border-violet-500 m-5 hover:border-violet-400 bg-white drop-shadow-lg'
+                <div className={border}
                 onDragOver={handleDragOver}
                 onDrop={handleDrop}>
                     <input type='file' multiple accept='.jpg, .pdf, .png' onChange={(event: any)=>{
