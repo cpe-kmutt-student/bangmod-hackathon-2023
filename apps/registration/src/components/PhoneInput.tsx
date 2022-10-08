9b9096c48d72ac5c26bbead9dc89131b4a7b051f
Chaiyapat Meeying <chaiyapatoam12@gmail.com>
2022-10-08 21:47:45 +0700

    fix maxLength

--- apps/registration/src/components/PhoneInput.tsx
+++ apps/registration/src/components/PhoneInput.tsx
@@ -48,7 +48,7 @@ const PhoneInput = ({
           onInput={handleChange}
           className="rounded-md appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm"
           required={required}
-          maxlength="10"
+          maxLength={10}
         />
         {isValid ? (
           <div></div>
