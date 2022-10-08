b039f90b1b5c04783feb84e9492168e6ea2f830d
Chaiyapat Meeying <chaiyapatoam12@gmail.com>
2022-10-08 21:37:39 +0700

    add required & add Event

--- apps/registration/src/components/PhoneInput.tsx
+++ apps/registration/src/components/PhoneInput.tsx
@@ -16,22 +16,28 @@ const PhoneInput = ({
 }) => {
   const [isValid, setIsValid] = useState<boolean>(true);
 
-  const handleChange = (e) => {
-    const phone = e.target.value;
+  const handleChange = (event: Event) => {
+    if (!(event.target instanceof HTMLInputElement)) return;
+    const phone = event.target.value;
     // if (phone.length > 10) return;
 
-    setObj(e.target.value);
+    setObj(event.target.value);
     setIsValid(phone.length == 10);
   };
 
   return (
     <div className="mb-6">
-      <label
-        htmlFor={id}
-        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-      >
-        {placeholder}
-      </label>
+      {name ? (
+        <label
+          for="default-input"
+          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+        >
+          {name}
+          {required ? <span className="text-pink-700">*</span> : <div />}
+        </label>
+      ) : (
+        <div />
+      )}
       <div className="mt-1">
         <input
           type="text"
@@ -39,7 +45,7 @@ const PhoneInput = ({
           id={id}
           placeholder={placeholder}
           value={obj}
-          onChange={handleChange}
+          onInput={handleChange}
           className="rounded-md appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm"
           required={required}
           maxlength="10"
