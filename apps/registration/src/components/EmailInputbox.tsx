3be7d7e3c19161608587e9abe162259ea8eaa9f5
sokungz01 <9.panuruj13@gmail.com>
2022-10-08 21:18:11 +0700

    change class to className , e to event

--- apps/registration/src/components/EmailInputbox.tsx
+++ apps/registration/src/components/EmailInputbox.tsx
@@ -16,7 +16,8 @@ const EmailInputbox = ({
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const [isValid, setIsValid] = useState<boolean>(false);
 
-  const handleChange = (event : Event) => {
+  const handleChange = (event: Event) => {
+    if (!(event.target instanceof HTMLInputElement)) return;
     const email = event.target.value;
     if (reg.test(email) === false) setIsValid(true);
     else setIsValid(false);
