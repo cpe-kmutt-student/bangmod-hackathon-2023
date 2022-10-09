08f5f7d526be3c0d6a04d2d3737fd1ce8c020bbf
riflowth <riflowth@gmail.com>
2022-10-09 21:04:17 +0700

    refactor(backend): add cors middleware for dev

---
+++ apps/backend/src/middleware/DevCorsMiddleware.ts
@@ -0,0 +1,20 @@
+import { Middleware, NextFunction, Request, Response, RouteHandler, RouteMetadata } from 'springpress';
+
+export class DevCorsMiddleware extends Middleware {
+
+  private static readonly DEV_ORIGINS: string[] = [
+    'http://localhost:3001',
+    'http://localhost:3002',
+  ];
+
+  public getHandler(routeMetadata: RouteMetadata): RouteHandler {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      if (req.headers.origin && DevCorsMiddleware.DEV_ORIGINS.includes(req.headers.origin)) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+      }
+      res.header('Access-Control-Allow-Credentials', 'true');
+      next();
+    };
+  }
+
+}
