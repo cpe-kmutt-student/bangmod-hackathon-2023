36202a58841d999739f0d66e4c1d10c0e03a12b9
riflowth <riflowth@gmail.com>
2022-10-09 15:03:11 +0700

    refactor: display auth data in /auth/me

--- apps/backend/src/controller/AuthController.ts
+++ apps/backend/src/controller/AuthController.ts
@@ -22,23 +22,22 @@ export class AuthController extends Controller {
   @RouteMapping('/callback', Methods.GET)
   private async oAuthCallback(req: Request<AuthGetCallbackApiSchema>, res: Response) {
     const { code } = req.query;
-    if (!code) throw new BadRequestException();
+    if (!code) throw new BadRequestException('Invalid authentication code');
 
     try {
       const user = await this.authService.getOAuthUser(code.toString());
-      await this.authService.setSession(res, user);
+      await this.authService.setSessionFromOAuth(res, user);
       res.redirect(process.env.FRONTEND_URL);
-    } catch {
-      throw new BadRequestException();
+    } catch (error: Error | unknown) {
+      const message = (error instanceof Error) ? error.message : 'Invalid user authentication';
+      throw new BadRequestException(message);
     }
   }
 
   @RequireAuth()
   @RouteMapping('/me', Methods.GET)
   private async me(req: Request, res: Response<AuthGetMeApiSchema>) {
-    res.status(200).json({
-      name: 'test'
-    });
+    res.status(200).json(req.session);
   }
 
 }
