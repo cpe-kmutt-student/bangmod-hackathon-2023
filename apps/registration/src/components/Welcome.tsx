7864ff093a406f895f0243fd1ca922698483d18b
Porping <porsteam@hotmail.com>
2022-10-10 23:38:44 +0700

    fix: remove unused component

--- apps/registration/src/components/Welcome.tsx
+++ apps/registration/src/components/Welcome.tsx
@@ -1,22 +1,7 @@
-import PhoneInput from "@/components/PhoneInput";
-import { useState } from "preact/hooks";
-
 export const Welcome = () => {
-  const [obj, setObj] = useState("");
-
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <div className="text-2xl">
-        Welcome to Registration
-        <PhoneInput
-          name="phone"
-          id="phone"
-          placeholder="please enter phone"
-          obj={obj}
-          setObj={setObj}
-          required={true}
-        />
-      </div>
+      <div className="text-2xl">Welcome to Registration</div>
     </div>
   );
 };
