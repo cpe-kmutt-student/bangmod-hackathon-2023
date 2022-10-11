9209028ec79f99c8d1ea31bafe8bd4354ca5388e
Qandle <unmistakework@hotmail.com>
2022-10-12 04:53:52 +0700

    feat(registration): redirect from wrong path

--- apps/registration/src/App.tsx
+++ apps/registration/src/App.tsx
@@ -1,15 +1,15 @@
-import { AuthProvider } from '@/contexts/AuthContext';
 import { HomePage } from '@/pages/HomePage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RegisterPage } from '@/pages/RegisterPage';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <AuthProvider>
-      <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/registration" element={<RegisterPage />} />
+        <Route path="*" element={<Navigate to="/" />} />        
+      </Routes>
+    </BrowserRouter>
   );
 };
