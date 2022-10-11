5917d86c0300562cc14bdfcff9d5e44d560d382e
Porping <porsteam@hotmail.com>
2022-10-11 22:16:04 +0700

    Create Team Form components  (#49)
    * feat: add prop for adjusting width and config same style
    
    * feat: add team form component
    
    * fix: delete id props in phone input
    
    * fix: remove unused component
    
    * fix: change props lable instead of prop name
    
    * fix: prop dropdown  and style same css
    
    * feat: change inputbox to dropdown component

--- apps/registration/src/components/EmailInputbox.tsx
+++ apps/registration/src/components/EmailInputbox.tsx
@@ -1,16 +1,21 @@
-import { useState } from "preact/hooks";
+import { TeamForm } from "@/components/TeamForm";
+import { StateUpdater, useState } from "preact/hooks";
 const EmailInputbox = ({
   obj,
   setObj,
   name,
+  label,
   placeholder,
   required,
+  width,
 }: {
   obj: string | number;
-  setObj: any;
-  name?: string;
+  setObj: StateUpdater<TeamForm>;
+  name: string;
+  label?: string;
   placeholder?: string;
   required?: boolean;
+  width?: string;
 }) => {
   const reg =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -21,18 +26,18 @@ const EmailInputbox = ({
     const email = event.target.value;
     if (reg.test(email) === false) setIsValid(true);
     else setIsValid(false);
-    setObj(email);
+    setObj((obj) => ({ ...obj, [name]: email }));
   };
 
   return (
     <>
-      <div className="mb-6">
-        {name ? (
+      <div className={`mb-6 ${width && width}`}>
+        {label ? (
           <label
             for="default-input"
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            className="block uppercase tracking-wide text-gray-700 text-sm md:text-2xl font-bold mb-2"
           >
-            {name}
+            {label}
             {required ? <span className="text-pink-700">*</span> : <div />}
           </label>
         ) : (
@@ -43,11 +48,11 @@ const EmailInputbox = ({
           placeholder={placeholder}
           value={obj}
           onInput={handleChange}
-          className="rounded-md appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm"
+          className="rounded-lg appearance-none relative block w-full px-5 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 text-sm md:text-2xl sm:text-sm"
         />
 
         {isValid ? (
-          <span className="text-red-700">Please enter valid email.</span>
+          <span className="text-red-500">Please enter valid email.</span>
         ) : (
           <label></label>
         )}
---
+++ apps/registration/src/components/TeamForm.tsx
@@ -0,0 +1,221 @@
+import DropDown from "@/components/DropDown";
+import EmailInputbox from "@/components/EmailInputbox";
+import ImageInputBox from "@/components/ImageInputBox";
+import Inputbox from "@/components/Inputbox";
+import PhoneInput from "@/components/PhoneInput";
+import { useState } from "preact/hooks";
+
+export type TeamForm = {
+  teamName: string;
+  school: string;
+  amount: number;
+  perfixTH: string;
+  nameTH: string;
+  middleNameTH: string;
+  surnameTH: string;
+  perfixEN: string;
+  nameEN: string;
+  middleNameEN: string;
+  surnameEN: string;
+  email: string;
+  phone: string;
+  line: string;
+};
+
+export const TeamForm = () => {
+  const [data, setData] = useState<TeamForm>({
+    teamName: "",
+    school: "",
+    amount: 0,
+    perfixTH: "",
+    nameTH: "",
+    middleNameTH: "",
+    surnameTH: "",
+    perfixEN: "",
+    nameEN: "",
+    middleNameEN: "",
+    surnameEN: "",
+    email: "",
+    phone: "",
+    line: "",
+  });
+
+  const [file, setFile] = useState<FileList | null>(null);
+
+  return (
+    <div className="w-full md:w-11/12 m-auto md:h-11/12 bg-gray-400 bg-opacity-60  rounded-xl">
+      <div className="px-6 py-6 md:py-9 md:px-12 md:space-y-3">
+        {/* first grid */}
+        <div className="flex flex-col justify-center items-center  md:flex-row md:justify-between md:space-x-5">
+          <Inputbox
+            obj={data.teamName}
+            setObj={setData}
+            name="teamName"
+            label="ชื่อทีม"
+            placeholder="Teams name"
+            required
+            width="w-full md:w-3/6"
+          />
+          <Inputbox
+            obj={data.school}
+            setObj={setData}
+            name="school"
+            label="โรงเรียน"
+            placeholder="School name"
+            required
+            width="w-full md:w-2/6"
+          />
+          <DropDown
+            obj={data.amount}
+            setObj={setData}
+            name="amount"
+            label="จำนวนสมาชิก"
+            options={[
+              { label: "1", value: 1 },
+              { label: "2", value: 2 },
+              { label: "3", value: 3 },
+            ]}
+            required
+            width="w-full md:w-1/6"
+          />
+        </div>
+        {/* Sensei */}
+        <div>
+          <h1 className="flex justify-center md:justify-start text-3xl md:text-4xl">
+            อาจารย์ที่ปรึกษา
+          </h1>
+        </div>
+        {/* Second grid */}
+        <div className="flex flex-col items-center md:flex-row justify-center  md:justify-between ">
+          <DropDown
+            obj={data.perfixTH}
+            setObj={setData}
+            name="perfixTH"
+            label="คำนำหน้า"
+            options={[
+              { label: "นาย", value: "นาย" },
+              { label: "นาง", value: "นาง" },
+              { label: "นางสาว", value: "นางสาว" },
+            ]}
+            required
+            width="w-full md:w-2/12"
+          />
+          <Inputbox
+            obj={data.nameTH}
+            setObj={setData}
+            name="nameTH"
+            label="ชื่อ (ภาษาไทย)"
+            placeholder="Name"
+            required
+            width="w-full md:w-3/12"
+          />
+          <Inputbox
+            obj={data.middleNameTH}
+            setObj={setData}
+            name="middleNameTH"
+            label="ชื่อกลาง (ภาษาไทย)"
+            placeholder="Middle name"
+            required
+            width="w-full md:w-3/12"
+          />
+          <Inputbox
+            obj={data.surnameTH}
+            setObj={setData}
+            name="surnameTH"
+            label="นามสกุล (ภาษาไทย)"
+            placeholder="Surname"
+            required
+            width="w-full md:w-3/12"
+          />
+        </div>
+        {/* Thrid grid */}
+        <div className="flex flex-col md:flex-row justify-center  md:justify-between ">
+          <DropDown
+            obj={data.perfixEN}
+            setObj={setData}
+            name="perfixTH"
+            label="Prefix"
+            options={[
+              { label: "Mr.", value: "Mr." },
+              { label: "Mrs.", value: "Mrs." },
+              { label: "Miss.", value: "Miss." },
+            ]}
+            required
+            width="w-full md:w-2/12"
+          />
+          <Inputbox
+            obj={data.nameEN}
+            setObj={setData}
+            name="nameEN"
+            label="Name"
+            placeholder="Name"
+            required
+            width="w-full md:w-3/12"
+          />
+          <Inputbox
+            obj={data.middleNameEN}
+            setObj={setData}
+            name="middleNameEN"
+            label="Middle name"
+            placeholder="Middle name"
+            required
+            width="w-full md:w-3/12"
+          />
+          <Inputbox
+            obj={data.surnameEN}
+            setObj={setData}
+            name="surnameEN"
+            label="Surname"
+            placeholder="Surname"
+            required
+            width="w-full md:w-3/12"
+          />
+        </div>
+        {/* Fourth grid */}
+        <div className="flex flex-col items-center md:flex-row justify-center  md:justify-between md:space-x-7">
+          <EmailInputbox
+            obj={data.email}
+            setObj={setData}
+            name="email"
+            label="Email"
+            placeholder="Email"
+            required
+            width="w-full md:w-2/6"
+          />
+          <PhoneInput
+            obj={data.phone}
+            setObj={setData}
+            name="phone"
+            label="เบอร์โทรศัพท์"
+            placeholder="Tel. number"
+            required
+            width="w-full md:w-2/6"
+          />
+          <Inputbox
+            obj={data.line}
+            setObj={setData}
+            name="line"
+            label="Line ID"
+            placeholder="ID Line"
+            required
+            width="w-full md:w-2/6"
+          />
+        </div>
+        {/* File system */}
+        <div className="flex space-y-2 md:space-y-0 flex-col md:flex-row justify-between">
+          {/* Left side */}
+          <div class="md:w-1/2">
+            <h1 className="text-sm md:text-3xl">แนบไฟล์เอกสาร</h1>
+            <p className="text-sm md:text-2xl">
+              1. หนังสือรับรองของอาจารย์ที่ปรึกษาตัวจริง
+            </p>
+          </div>
+          {/* Right side */}
+          <div class="md:w-1/2 h-40 relative">
+            <ImageInputBox obj={file} setObj={setFile} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
