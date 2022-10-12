db0ed479e39599bfa5e4be1bea45f591665fedc2
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2022-10-12 22:25:14 +0700

    change color follow ref figma

--- apps/registration/src/components/Card.tsx
+++ apps/registration/src/components/Card.tsx
@@ -3,25 +3,28 @@ const Card = ({
   title,
   desc1,
   desc2,
+  desc3,
 }: {
   img?: string;
   title?: string;
   desc1?: string;
   desc2?: string;
+  desc3?: string;
 }) => {
   return (
     <>
       <div className="w-full max-w-sm mb-5 bg-white rounded-3xl shadow-2xl">
-        <a href="#">
+        <div className="max-w-md mx-auto rounded-full bg-[#e15a47] w-1/2 mb-8 mt-8 sm:mb-8 sm:mt-8 lg:mb-8 lg:mt-8">
           <img
             className="p-8 rounded-t-lg w-full"
             src= {img}
           />
-        </a>
-        <div className="flex flex-col items-center pb-6">
+        </div>
+        <div className="flex flex-col items-center pb-6 text-center">
             <p className="font-bold text-purple-900 lg:text-base "> {title} </p>
             <p className="md:text-xs font-light text-purple-700 "> {desc1} </p>
             <p className="md:text-xs font-light text-purple-700 "> {desc2} </p>
+            <p className="md:text-xs font-light text-purple-700 "> {desc3} </p>
         </div>
       </div>
     </>
--- apps/registration/src/components/Reward.tsx
+++ apps/registration/src/components/Reward.tsx
@@ -3,34 +3,34 @@ import Card from "@/components/Card";
 export const Reward = () => {
   return (
     <>
-      <div className="p-20 grid sm:grid-cols-1 md:grid-cols-2 md:gap-4 lg:gap-4 lg:grid-cols-4 xl:grid-cols-4 xl:gap-8">
+      <div className="p-20 grid sm:grid-cols-1 md:grid-cols-2 md:gap-4 lg:gap-4 lg:grid-cols-4 xl:grid-cols-4 xl:gap-8 -mb-16">
 
         <div className="md:col-start-1 md:col-end-3 lg:col-start-4 lg:col-end-4 max-w-full mb-2">
-          <span className="px-4 py-4 bg-amber-500 rounded-lg text-2xl block text-white font-bold text-center shadow-xl">
+          <span className="px-4 py-4 bg-[#DB9116] rounded-3xl text-2xl block text-white font-bold text-center shadow-xl">
             รางวัล{" "}
           </span>
 
         </div>
         <Card
-          img="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/medal-design-template-89182c980b9f3214cca1dca00b3d27b4_screen.jpg?ts=1621433255"
+          img="https://cdn-icons-png.flaticon.com/512/1157/1157014.png"
           title="รางวัลชนะเลิศ"
           desc1="30,000 บาท"
           desc2="พร้อมโล่ประกาศเกียรติคุณ"
         />
         <Card
-          img="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/medal-design-template-89182c980b9f3214cca1dca00b3d27b4_screen.jpg?ts=1621433255"
+          img="https://cdn-icons-png.flaticon.com/512/1157/1157014.png"
           title="รางวัลรองชนะเลิศอันดับหนึ่ง"
           desc1="15,000 บาท"
           desc2="พร้อมโล่ประกาศเกียรติคุณ"
         />
         <Card
-          img="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/medal-design-template-89182c980b9f3214cca1dca00b3d27b4_screen.jpg?ts=1621433255"
+          img="https://cdn-icons-png.flaticon.com/512/1157/1157014.png"
           title="รางวัลรองชนะเลิศอันดับสอง"
           desc1="15,000 บาท"
           desc2="พร้อมโล่ประกาศเกียรติคุณ"
         />
         <Card
-          img="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/medal-design-template-89182c980b9f3214cca1dca00b3d27b4_screen.jpg?ts=1621433255"
+          img="https://cdn-icons-png.flaticon.com/512/1157/1157014.png"
           title="รางวัลชมเชย"
           desc1="15,000 บาท"
           desc2="พร้อมโล่ประกาศเกียรติคุณ"
