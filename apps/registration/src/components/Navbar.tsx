684759fa928092b0170913d4142690d5847df57f
Qandle <unmistakework@hotmail.com>
2022-10-12 23:57:11 +0700

    feat(registration): navbar responsive for mobile

--- apps/registration/src/components/Navbar.tsx
+++ apps/registration/src/components/Navbar.tsx
@@ -14,7 +14,7 @@ export const NavElement = ({
   !className ? className = "" : className
   console.log(className)
   return (
-    <a href={to} className={className + " flex-auto flex justify-center items-center border-y-[0.01px] border-white/20"}>{text}</a>
+    <a href={to} className={className + " flex-auto flex justify-center items-center md:border-y-[0.01px] border-white/20"}>{text}</a>
   );
 };
 
@@ -23,16 +23,25 @@ export const Navbar = () => {
 
   return (
     <div className="fixed w-full h-16 md:h-30 z-10">
-      <div className="static md:hidden w-full h-full p-0 m-0 flex justify-end items-center z-10 pr-5">
+      <div className="static md:hidden w-full h-full bg-[#3E245D]/70 backdrop-blur-md md:bg-transparent p-0 m-0 flex justify-end items-center z-10 pr-5">
         <button
           className="p-2 rounded-md"
           onClick={() => setNavbar(!navbar)}
         >
-          <div className="b h-[1rem] w-[1.3125rem] flex flex-col items-center justify-between">
-            <div className="h-0 border-t-[0.13rem] border-white w-[1.3125rem] rounded-md" />
-            <div className="h-0 border-t-[0.13rem] border-white w-[1.3125rem] rounded-md" />
-            <div className="h-0 border-t-[0.13rem] border-white w-[1.3125rem] rounded-md" />
-          </div>
+          { navbar ? (
+            <div className="b h-[1rem] w-[1.3125rem] flex flex-col items-center justify-between">
+              <div className="h-0 border-t-[0.13rem] border-white w-[1.3125rem] rounded-md" />
+              <div className="h-0 border-t-[0.13rem] border-white w-[1.3125rem] rounded-md" />
+              <div className="h-0 border-t-[0.13rem] border-white w-[1.3125rem] rounded-md" />
+            </div>
+          ) :
+          (
+              <div className="relative b h-[1rem] w-[1.3125rem] flex flex-col items-center justify-center md:justify-between">
+                <div className="absolute h-0 border-t-[0.2rem] border-white w-[1.3125rem] rounded-md rotate-45" />
+                <div className="absolute h-0 border-t-[0.2rem] border-white w-[1.3125rem] rounded-md rotate-[-45deg]" />
+              </div>
+          )
+          }
         </button>
       </div>
       <div className="static hidden md:flex justify-center items-center">
@@ -47,6 +56,21 @@ export const Navbar = () => {
           <NavLink to="/registration" className="bg-[#DB9116] flex-auto flex justify-center items-center rounded-r-md border-[0.01px] border-white"> สมัคร </NavLink>
         </nav>
       </div>
+      {
+        !navbar && (
+          <div className="static flex justify-center items-center md:hidden">
+            <nav className="w-full h-[20rem] flex flex-col bg-[#3E245D]/70 backdrop-blur-md p-0 text-white pb-5">
+              <NavElement text="รายละเอียด" to="#" />
+              <NavElement text="คุณสมบัติ" to="#" />
+              <NavElement text="รางวัล" to="#" />
+              <NavElement text="ไทม์ไลน์" to="#" />
+              <NavElement text="ขอบเขตเนื้อหา" to="#" />
+              <NavElement text="ติดต่อสอบถาม" to="#" />
+              <NavLink to="/registration" className="bg-[#8B69AE] flex-auto flex justify-center items-center border-[0.01px] border-white rounded-md m-2 mx-20"> สมัคร </NavLink>
+            </nav>
+          </div>
+        )
+      }
     </div>
   );
 };
