2d5033a2cc81bacdfa07324e0455998d6252646b
riflowth <riflowth@gmail.com>
2022-10-14 21:47:03 +0700

    refactor: add more gap between sections

--- apps/registration/src/components/ScopeSection.tsx
+++ apps/registration/src/components/ScopeSection.tsx
@@ -11,7 +11,7 @@ export const ScopeSection = () => {
   }, [isVisible]);
 
   return (
-    <section ref={ref} id="scope-section" className="h-full py-14">
+    <section ref={ref} id="scope-section" className="h-full py-24">
       <div className="w-fit mx-auto mb-8 px-16 py-4 bg-yellow-600 rounded-xl text-base md:text-2xl text-white font-bold shadow-xl">
         ขอบเขตเนื้อหาที่ใช้ในการแข่งขัน
       </div>
