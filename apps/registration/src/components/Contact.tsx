6d47e083f9786487cf34753f7036b17b6c759125
khemmaphat <khemmaphat.work@gmail.com>
2022-10-15 00:00:11 +0700

    fix NEED HELP?

--- apps/registration/src/components/Contact.tsx
+++ apps/registration/src/components/Contact.tsx
@@ -2,10 +2,10 @@ export const Contact = () => {
     return (
         <section className="h-full py-12">
             <div>
-                <div className="w-fit mx-auto mb-8 px-16 py-4 rounded-xl text-2xl text-white font-bold">
-                    NEED HELP?
+                <div className="text-6xl text-center text-white font-bold">
+                    <h1>NEED HELP?</h1>
                 </div>
-                <div className="grid grid-cols-2 gap-4 md:grid-cols-2 text-white lg:grid-cols-4 xl:grid-cols-4 xl:gap-8">
+                <div className="grid">
                     <img class='md:h-full md:w-48' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/600px-Facebook_Logo_%282019%29.png'></img>
                     <p className="font-bold text-white lg:text-base" > BANGMOD HACKTHON 2023</p>
                     <img src="https://www.clipartmax.com/png/full/2-23363_icon-telephone-png.png" width={74} height={74}></img>
