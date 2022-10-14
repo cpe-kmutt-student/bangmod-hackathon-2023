87dd14dc2e281c19d53231cf99b1b899dc2d89e7
riflowth <riflowth@gmail.com>
2022-10-14 12:28:11 +0700

    fix: wrong registration component route

--- apps/registration/src/App.tsx
+++ apps/registration/src/App.tsx
@@ -1,7 +1,7 @@
 import { AuthProvider } from '@/contexts/AuthContext';
 import { HomePage } from '@/pages/HomePage';
-import { RegisterPage } from '@/pages/RegisterPage';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { RegistrationPage } from '@/pages/RegistrationPage';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
@@ -9,7 +9,7 @@ export const App = () => {
       <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/register" element={<RegistrationPage />} />
           <Route path="*" element={<Navigate to="/" />} />        
         </Routes>
       </BrowserRouter>
---
+++ apps/registration/src/pages/RegistrationPage.tsx
@@ -0,0 +1,7 @@
+import { TeamForm } from '@/components/TeamForm';
+
+export const RegistrationPage = () => {
+  return (
+    <TeamForm />
+  );
+};
