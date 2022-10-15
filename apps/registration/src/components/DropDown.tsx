77d54234a4046c8e049571ea458c1c9db4fbf416
riflowth <riflowth@gmail.com>
2022-10-16 02:31:08 +0700

    fix: dropdown arrow icon

--- apps/registration/src/components/DropDown.tsx
+++ apps/registration/src/components/DropDown.tsx
@@ -44,11 +44,12 @@ const DropDown = <T,>({
       ) : (
         <div />
       )}
+
       <select
         value={obj}
         required={required}
         onChange={handleChange}
-        className="p-2 md:pl-2 md:p-1 relative block w-full appearance-none rounded-md border border-gray-300  pl-2 text-black placeholder-[#b597d1] drop-shadow-md focus:z-10 focus:border-purple-500 focus:outline-none focus:ring-purple-500"
+        className="p-2 md:p-1 md:px-2 md:pr-6 block w-full rounded-md border border-gray-300 text-black placeholder-[#b597d1] drop-shadow-md focus:z-10 focus:border-purple-500 focus:outline-none focus:ring-purple-500"
       >
         {options.map((e: option) => (
           <option value={e.value}>{e.label}</option>
