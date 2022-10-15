be3a33f4b7f631864d21385b7517dfacdfa6163a
XiaoXuxxxx <aonrok555@gmail.com>
2022-10-15 12:15:38 +0700

    feat: make it more responsive

--- apps/registration/src/components/RegistrationForm.tsx
+++ apps/registration/src/components/RegistrationForm.tsx
@@ -25,7 +25,9 @@ export const RegistrationForm = () => {
 
   return (
     <div className="flex h-full items-center justify-center">
-      <div className="m-5 flex w-full flex-col sm:w-4/5 md:w-3/5">
+      <div className="m-5 flex w-full flex-col sm:w-5/6 md:w-4/5 lg:w-3/5">
+
+      {/* <div className="m-5 flex w-full flex-col sm:w-4/5 md:w-3/5"> */}
         <RegisterHeader />
 
         <form onSubmit={handleSubmit}>
