bb3d0922cd1eb688271f3faad6d61cc02938853e
XiaoXuxxxx <aonrok555@gmail.com>
2022-10-15 11:28:47 +0700

    feat: input can handle updater more dynamically

--- apps/registration/src/components/DropDown.tsx
+++ apps/registration/src/components/DropDown.tsx
@@ -1,11 +1,11 @@
-import { TeamForm } from "@/components/TeamForm";
 import { StateUpdater } from "preact/hooks";
+
 type option = {
   label?: string | number;
   value?: string | number;
 };
 
-const DropDown = ({
+const DropDown = <T,>({
   obj,
   setObj,
   options,
@@ -13,38 +13,49 @@ const DropDown = ({
   label,
   required,
   width,
+  index,
 }: {
   obj?: string | number;
-  setObj: StateUpdater<TeamForm>;
+  setObj: StateUpdater<T>;
   name: string;
   label?: string;
   options: option[];
   required?: boolean;
   width?: string;
+  index?: number;
 }) => {
   const handleChange = (event: Event) => {
     if (!(event.target instanceof HTMLSelectElement)) return;
     const value = event.target.value;
 
-    setObj((obj) => ({ ...obj, [name]: value }));
+    setObj((prev) => {
+      const newObj =
+        index !== undefined
+          ? { [index]: { ...prev[index as keyof T], [name]: value } }
+          : { [name]: value };
+
+      return { ...prev, ...newObj };
+    });
   };
+
   return (
-    <div className={`mb-6 ${width && width}`}>
+    <div className={`${width && width} mb-6 flex flex-col md:mb-0`}>
       {label ? (
         <label
           for="default-input"
-          className="block uppercase tracking-wide text-gray-700 text-sm md:text-2xl font-bold mb-2"
+          className="mb-1 block pl-2 tracking-wide text-white "
         >
           {label}
-          {required ? <span className="text-pink-700">*</span> : <div />}
+          {required ? <span className="text-[#ffdc19]">*</span> : <div />}
         </label>
       ) : (
         <div />
       )}
       <select
         value={obj}
+        required={required}
         onChange={handleChange}
-        className="rounded-lg appearance-none relative block w-full px-5 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 text-sm md:text-2xl sm:text-sm"
+        className="relative block w-full appearance-none rounded-md border border-gray-300 p-1 pl-2 text-black placeholder-gray-500 drop-shadow-md focus:z-10 focus:border-purple-500 focus:outline-none focus:ring-purple-500"
       >
         {options.map((e: option) => (
           <option value={e.value}>{e.label}</option>
--- apps/registration/src/components/PhoneInput.tsx
+++ apps/registration/src/components/PhoneInput.tsx
@@ -1,6 +1,5 @@
-import { TeamForm } from "@/components/TeamForm";
 import { StateUpdater, useState } from "preact/hooks";
-const PhoneInput = ({
+const PhoneInput = <T,>({
   name,
   placeholder,
   obj,
@@ -13,7 +12,7 @@ const PhoneInput = ({
   label?: string;
   placeholder: string;
   obj: string | number;
-  setObj: StateUpdater<TeamForm>;
+  setObj: StateUpdater<T>;
   required?: boolean;
   width?: string;
 }) => {
@@ -28,14 +27,14 @@ const PhoneInput = ({
   };
 
   return (
-    <div className={`mb-6 ${width && width}`}>
+    <div className={`mb-6 ${width && width} mb-6 flex flex-col md:mb-0`}>
       {label ? (
         <label
           for="default-input"
-          className="block uppercase tracking-wide text-gray-700 text-sm md:text-2xl font-bold mb-2"
+          className="mb-1 block text-sm uppercase tracking-wide text-white "
         >
           {label}
-          {required ? <span className="text-pink-700">*</span> : <div />}
+          {required ? <span className="text-[#ffdc19]">*</span> : <div />}
         </label>
       ) : (
         <div />
@@ -47,14 +46,14 @@ const PhoneInput = ({
           placeholder={placeholder}
           value={obj}
           onInput={handleChange}
-          className="rounded-lg appearance-none relative block w-full px-5 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 text-sm md:text-2xl sm:text-sm"
+          className="relative block w-full appearance-none rounded-md border border-gray-300 p-1 pl-2 text-black placeholder-[#b597d1] drop-shadow-md focus:z-10 focus:border-purple-500 focus:outline-none focus:ring-purple-500"
           required={required}
           maxLength={10}
         />
         {isValid ? (
           <div></div>
         ) : (
-          <span className="text-sm text-red-500">Please Enter Phone</span>
+          <span className="text-sm text-red-500 z-5">Please Enter Phone</span>
         )}
       </div>
     </div>
