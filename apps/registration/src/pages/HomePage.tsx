879ae7cb9eaf1b3e0aba690eb72c49818e05b70d
riflowth <riflowth@gmail.com>
2022-10-15 16:28:58 +0700

    refactor: change some parallax behaviours

--- apps/registration/src/pages/HomePage.tsx
+++ apps/registration/src/pages/HomePage.tsx
@@ -1,27 +1,40 @@
+import { ContactSection } from '@/components/ContactSection';
 import { Hero } from '@/components/Hero';
 import InformationSection from "@/components/InformationSection";
-import { Navbar } from "@/components/Navbar";
+import { Navbar } from '@/components/Navbar';
 import { Qualification } from "@/components/Qualification";
 import { Reward } from "@/components/Reward";
 import { ScopeSection } from "@/components/ScopeSection";
-import { ContactSection } from '@/components/ContactSection';
 
 export const HomePage = () => {
   return (
-    <div className="h-full bg-[#8C4380] overflow-hidden">
+    <div className="w-full h-full overflow-hidden">
       <Navbar />
-
+      
       <Hero />
 
-      <img className="hidden md:block 2xl:hidden relative transform translate-y-2 z-30 pt-96 overflow-hidden" src="bg2.webp" alt="" />
+      <div className="w-full h-full">
+        <img
+          src="bg2.webp"
+          alt=""
+          className="
+            w-full absolute bottom-0 z-20 transform translate-y-2 md:translate-y-[6rem]
+          " 
+        />
 
-      <div className="relative bg-gradient-to-b from-[#3E245D] via-[#B04F9C] to-[#40245E] z-40">
-        <div className="w-full container space-y-24 py-24">
-          <InformationSection />
-          <Qualification />
-          <Reward />
-          <ScopeSection />
-          <ContactSection />
+        <div
+          className="
+            relative bg-gradient-to-b from-[#3E245D] via-[#B04F9C] to-[#40245E] z-30
+            transform md:translate-y-[5.9rem]
+          "
+        >
+          <div className="w-full container space-y-24 py-24">
+            <InformationSection />
+            <Qualification />
+            <Reward />
+            <ScopeSection />
+            <ContactSection />
+          </div>
         </div>
       </div>
     </div>
