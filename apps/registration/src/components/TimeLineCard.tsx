83ab5cc7776958b318bfe2959ea8e221ea12dc1a
Porping <porsteam@hotmail.com>
2022-10-17 20:13:27 +0700

    fix:change width and color of timeline card

--- apps/registration/src/components/TimeLineCard.tsx
+++ apps/registration/src/components/TimeLineCard.tsx
@@ -6,16 +6,14 @@ export const TimeLineCard = ({
   date: string;
 }) => {
   return (
-    <div>
-      <div className="flex w-full  justify-center items-center">
-        {/* Card */}
-        <div className=" bg-white rounded-xl p-2 space-y-2 md:space-y-5">
-          <div className=" text-center px-4 py-5 bg-violet-600 rounded-xl text-xs md:text-xl text-white font-bold">
-            {title}
-          </div>
-          <div className="flex justify-center text-xs text-violet-600 md:text-lg">
-            {date}
-          </div>
+    <div className="flex w-full h-48 justify-center items-center">
+      {/* Card */}
+      <div className="w-full bg-white rounded-xl p-2 space-y-2 md:space-y-5">
+        <div className=" text-center px-4 py-5 bg-violet-400 rounded-xl text-xs md:text-xl text-white font-bold">
+          {title}
+        </div>
+        <div className="flex justify-center text-xs text-violet-600 md:text-lg">
+          {date}
         </div>
       </div>
     </div>
