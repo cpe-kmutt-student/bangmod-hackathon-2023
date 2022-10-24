dad52305b09ef854e1801723ee895cdfd6b02e6e
riflowth <riflowth@gmail.com>
2022-10-24 20:53:45 +0700

    hotfix: wrong checker key
    Co-authored-by: Shinnapat Koparamestrisin <PPHamster@users.noreply.github.com>

--- apps/backend/src/service/InputService.ts
+++ apps/backend/src/service/InputService.ts
@@ -144,14 +144,14 @@ export class InputService {
     }
 
     for (const key in RegistrationFormDataTemplate.advisor) {
-      if (!['midddleNameEn', 'middleNameTh'].includes(key) && !advisor[key]) return false;
+      if (!['middleNameEn', 'middleNameTh'].includes(key) && !advisor[key]) return false;
     }
 
     if (!Array.isArray(students) || students.length < 2 || students.length > 3) return false;
 
     for (const student of students) {
       for (const key in RegistrationFormDataTemplate.students[0]) {
-        if (!['midddleNameEn', 'middleNameTh', 'foodType', 'foodAllergy', 'drugAllergy', 'disease'].includes(key) && !student[key]) return false;
+        if (!['middleNameEn', 'middleNameTh', 'foodType', 'foodAllergy', 'drugAllergy', 'disease'].includes(key) && !student[key]) return false;
       }
     }
 
